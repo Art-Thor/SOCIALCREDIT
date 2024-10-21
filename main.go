4ce32229009ee3f65baf10c7a9a9af5c4ade2a37
Arthur Holubov <arthurholubov@gmail.com>
2024-10-21 12:59:37 +0300

    prisoner feature

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math"
 	"os"
 	"os/signal"
 	"strings"
@@ -28,6 +27,7 @@ type Words struct {
 var words Words
 var token string
 var userCredits = make(map[string]int)
+var originalNicknames = make(map[string]string)
 
 func main() {
 	err := godotenv.Load(".env")
@@ -110,7 +110,7 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		GuildID:   m.GuildID,
 	}
 
-	// Check bad words
+	// Check for bad words
 	for _, word := range words.Bad {
 		if strings.Contains(contentLower, strings.ToLower(word.Text)) {
 			score -= word.Price
@@ -118,7 +118,7 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 	}
 
-	// Check good words
+	// Check for good words
 	for _, word := range words.Good {
 		if strings.Contains(contentLower, strings.ToLower(word.Text)) {
 			score += word.Price
@@ -133,17 +133,83 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	saveUserCredits()
 
-	// Check thresholds
-	prevThreshold := int(math.Floor(float64(prevCredit) / -1000.0))
-	newThreshold := int(math.Floor(float64(newCredit) / -1000.0))
+	// Threshold for changing nickname
+	negativeThreshold := -1000
 
-	if newThreshold > prevThreshold {
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s, you have reached %d social credit! Threatening to send to gulag!", m.Author.Mention(), newCredit))
+	// Check if the user has reached the negative threshold
+	if newCredit <= negativeThreshold && prevCredit > negativeThreshold {
+		// Save the original nickname if it hasn't been saved yet
+		if _, exists := originalNicknames[userID]; !exists {
+			member, err := s.GuildMember(m.GuildID, userID)
+			if err != nil {
+				log.Printf("Error fetching guild member: %v", err)
+				return
+			}
+			if member.Nick != "" {
+				originalNicknames[userID] = member.Nick
+			} else {
+				originalNicknames[userID] = member.User.Username
+			}
+		}
+
+		// Generate a unique nickname "prisoner #N"
+		prisonerNumber := 1
+		existingNicknames := make(map[string]bool)
+
+		// Get the list of all server members
+		after := ""
+		for {
+			members, err := s.GuildMembers(m.GuildID, after, 1000)
+			if err != nil {
+				log.Printf("Error fetching guild members: %v", err)
+				break
+			}
+			if len(members) == 0 {
+				break
+			}
+			for _, member := range members {
+				nick := member.Nick
+				if nick == "" {
+					nick = member.User.Username
+				}
+				existingNicknames[nick] = true
+				after = member.User.ID
+			}
+			if len(members) < 1000 {
+				break
+			}
+		}
+
+		// Find a unique prisoner number
+		for {
+			newNickname := fmt.Sprintf("prisoner #%d", prisonerNumber)
+			if !existingNicknames[newNickname] {
+				break
+			}
+			prisonerNumber++
+		}
+
+		// Change the user's nickname
+		err := s.GuildMemberNickname(m.GuildID, userID, fmt.Sprintf("prisoner #%d", prisonerNumber))
+		if err != nil {
+			log.Printf("Error changing nickname: %v", err)
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s has been renamed to prisoner #%d", m.Author.Mention(), prisonerNumber))
 	}
 
-	// Check for very low rating
-	if newCredit <= -5000 {
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s, the prisoner has escaped! Return to the cell!", m.Author.Mention()))
+	// Check if the user has been rehabilitated
+	if newCredit > negativeThreshold && prevCredit <= negativeThreshold {
+		if originalNickname, exists := originalNicknames[userID]; exists {
+			err := s.GuildMemberNickname(m.GuildID, userID, originalNickname)
+			if err != nil {
+				log.Printf("Error restoring nickname: %v", err)
+				return
+			}
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s has been rehabilitated, and their nickname has been restored.", m.Author.Mention()))
+			delete(originalNicknames, userID)
+		}
 	}
 
 	// Check for the word "diggers"
